8a9d16d2fe18b48e13df6252ed805693df57ccb5
unknown <ilyavusnya@gmail.com>
2019-09-20 11:23:34 +0300

    Added all files to git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { StoreModule } from "../store/store.module";
+import { AppComponent } from "./app.component";
+import { StoreComponent } from "../store/store.component";
+import { CheckoutComponent } from "../store/checkout.component";
+import { CartDetailComponent } from "../store/cartDetail.component";
+import { RouterModule } from "@angular/router";
+import { StoreFirstGuard } from "./storeFirst.guard";
 
 @NgModule({
   declarations: [
@@ -12,9 +17,30 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    StoreModule,
+    RouterModule.forRoot([
+      {
+         path: "store", component: StoreComponent,
+         canActivate: [StoreFirstGuard]
+      },
+      {
+         path: "cart", component: CartDetailComponent,
+         canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "checkout", component: CheckoutComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "admin",
+        loadChildren: "../admin/admin.module#AdminModule",
+        canActivate: [StoreFirstGuard]
+      },
+      { path: "**", redirectTo: "/store" }
+    ])
   ],
-  providers: [],
+  providers: [StoreFirstGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
